4a19a5a9a9514f6f2a4c9a7dcad96434a20a9d31
HakonHystad <hakonhystad@gmail.com>
2017-03-27 17:52:27 +0200

    some debugging done

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ Section:                                          ~libs
 
 #include "hdmiLCD.h"
 #include <string>
+#include <iostream>
 
 /*#############################################################################################################
 
@@ -23,11 +24,21 @@ Section:                                          ~main
 
 int main()
 {
+    /*
+    std::cout << "Main\n";
     HdmiLCD LCD( 0x27 );
+    std::cout << "Made LCD\n";
+    std::string test = "test";
+    LCD.init();
+    LCD.print( test );
+    std::cout << "Printed\n";
+    */
+
+    auto LCD = new HdmiLCD( 0x27 );
 
     std::string test = "test";
-
-    LCD.print( test );
+    LCD->init();
+    LCD->print( test );
     
     return 0;
 }
